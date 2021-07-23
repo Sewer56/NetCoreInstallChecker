57787e1da7387ec5992ab55582ca5056c33fda52
Sewer 56 <seweratwork@gmail.com>
2021-07-23 23:37:06 +0100

    Added: Support for Direct EXE download.

---
+++ NetCoreInstallChecker/NetCoreInstallChecker/Structs/Config/Enum/Format.cs
@@ -0,0 +1,18 @@
+﻿namespace NetCoreInstallChecker.Structs.Config.Enum
+{
+    /// <summary>
+    /// Defines the format of the downloaded file.
+    /// </summary>
+    public enum Format
+    {
+        /// <summary>
+        /// File is an archive (zip or tar).
+        /// </summary>
+        Archive,
+
+        /// <summary>
+        /// File is an executable.
+        /// </summary>
+        Executable
+    }
+}
